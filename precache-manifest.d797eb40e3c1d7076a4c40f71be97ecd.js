226a49e64a0a3724948f740d1f509a3592e75326
Jon Shaw <jonshawtech@gmail.com>
2020-04-04 13:24:38 -0400

    Updates

---
+++ precache-manifest.d797eb40e3c1d7076a4c40f71be97ecd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f50f3194f85fe7fb301de024e8b8f0",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "dfdccdcb05e500df6a35",
+    "url": "/covid19tracker/static/css/main.8365ebf6.chunk.css"
+  },
+  {
+    "revision": "afff9873980aec345800",
+    "url": "/covid19tracker/static/js/2.95ef3b9e.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/covid19tracker/static/js/2.95ef3b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfdccdcb05e500df6a35",
+    "url": "/covid19tracker/static/js/main.620f129f.chunk.js"
+  },
+  {
+    "revision": "ab6e238057f2e9e24af7",
+    "url": "/covid19tracker/static/js/runtime-main.afba91d4.js"
+  }
+]);
